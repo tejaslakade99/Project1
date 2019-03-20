a664449d15c8259575314b49dee8d1d38d2c94dd
pratikVirkhare <pratikvirkhare4@gmail.com>
2019-03-19 22:42:53 -0500

    Branch ITR 52: Master Project - git_Project1 - Committing ITR52 Code

---
+++ src/test/java/automation/Practise_Project1/ITR52_Test.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package automation.Practise_Project1;
+
+/**
+ * @author Pratik
+ *
+ */
+public class ITR52_Test {
+
+	
+	public void Method1_ITR52()
+	{
+		System.out.println("Master Project: git_Project1_Master: ITR 52 Method 1");
+	}
+	
+	public void Method2_ITR52()
+	{
+		System.out.println("Master Project: git_Project1_Master: ITR 52 Method 2");
+	}
+	
+	public void Method3_ITR52()
+	{
+		System.out.println("Master Project: git_Project1_Master: ITR 52 Method 3");
+	}
+	
+	public void Method4_ITR52()
+	{
+		System.out.println("Master Project: git_Project1_Master: ITR 52 Method 4");
+	}
+	
+	public void Method5_ITR52()
+	{
+		System.out.println("Master Project: git_Project1_Master: ITR 52 Method 5");
+	}
+}
